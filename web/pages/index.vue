10328d33ab720fe5a4eb365d686aec0829b6c2b7
Nirjan Khadka <webdev.nk@gmail.com>
2025-02-25 10:29:16 +0545

    feat: :lipstick: show loading state while loading system overview

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <h1 class="lg:text-xl text-lg font-semi-bold">All available systems</h1>
-    <div class="grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 gap-4 py-6">
+    <div
+      v-if="!isLoading"
+      class="grid md:grid-cols-2 grid-cols-1 lg:grid-cols-3 gap-4 py-6"
+    >
       <div v-for="system in systems">
         <SystemOverview
           v-if="systemMetricsStreams[system]?.at(-1)"
@@ -9,19 +12,24 @@
           :link="`/systems/${system}`"
           :metrics="systemMetricsStreams[system].at(-1)!"
         />
+        <USkeleton class="h-60 w-full" v-else />
       </div>
     </div>
+
+    <USkeleton v-else class="h-60 w-full my-6" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import type { Metrics } from "../../shared/types";
+const isLoading = ref(false);
 
-const systems = (await $fetch("/api/sytems")) ?? [];
+const systems = ref([]);
+
 const systemMetricsStreams = reactive<Record<string, Metrics[]>>({});
 
 function setupMetricsStream() {
-  systems.forEach((system) => {
+  systems.value.forEach((system) => {
     const metricsStream = new EventSource("/api/metrics-stream?name=" + system);
 
     metricsStream.onmessage = (event) => {
@@ -32,8 +40,20 @@ function setupMetricsStream() {
   });
 }
 
-onMounted(() => {
-  setupMetricsStream();
+async function loadSystems() {
+  systems.value = (await $fetch("/api/sytems")) ?? [];
+}
+
+onMounted(async () => {
+  try {
+    isLoading.value = true;
+    await loadSystems();
+    setupMetricsStream();
+  } catch (error) {
+    console.log({ error });
+  } finally {
+    isLoading.value = false;
+  }
 });
 </script>
 
